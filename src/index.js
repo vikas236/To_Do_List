60d00335879ff5e837122ae3016fdd47a4e35580
sai vikas <saivikas461@gmail.com>
2023-06-15 05:02:58 +0530

    add basic navbar without functioning

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import './style.css';
 import './css/navbar.css';
 
+// import images 
+import msgs_icon from './icons/messages.png'
+
+
 // Calling html elements
 const body = document.querySelector("body");
 const search = document.querySelector(".search");
 const mag_glass = document.querySelector(".mag_glass");
 const search_box = search.childNodes[3];
+const message_icon = document.querySelector(".message_icon");
+
+
+message_icon.src = msgs_icon;
 
 search_box.addEventListener("mouseover", function() {
     (mag_glass.childNodes[3]).style.fill = "rgba(0, 0, 0, .4)";
